2f016ce4b13b1614c53b1cf415bd24cc656ce5f4
ume725323 <umemal2022@gmail.com>
2026-05-24 18:00:12 +0900

    じゃんぷしたよ

---
+++ practice/ume/0517/script.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+
+// キャラクター画像の定義
+const character = new Image();
+character.src = "../images/character_stand.png";
+
+// 画面幅・高さの定義
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+const GROUND_HEIGHT = 150;
+const SKY_HEIGHT = CANVAS_HEIGHT - GROUND_HEIGHT;
+const CHARACTER_WIDTH = 85;
+const CHARACTER_HEIGHT = 60;
+
+// 色の定義
+const COLOR_SKY = "#8adaff";
+const COLOR_GROUND = "#7eff5e";
+const COLOR_CLEAR = "#ffffff";
+
+
+let frame = 0;
+let x = 0;
+function tick() {
+    frame++;
+    x = (x + frame / 100) % CANVAS_WIDTH;
+
+    // (x: 0, y: 0) から画面サイズの四角形で塗りつぶし
+    ctx.fillStyle = COLOR_SKY;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // 地面の描画
+    ctx.fillStyle = COLOR_GROUND;
+    ctx.fillRect(0, SKY_HEIGHT, CANVAS_WIDTH, GROUND_HEIGHT);
+
+    // キャラクター画像の描画
+    ctx.drawImage(character, x, SKY_HEIGHT - CHARACTER_HEIGHT, CHARACTER_WIDTH, CHARACTER_HEIGHT);
+
+    // 再描画のリクエスト
+    requestAnimationFrame(tick);
+}
+
+// 初期描画のリクエスト
+requestAnimationFrame(tick)
