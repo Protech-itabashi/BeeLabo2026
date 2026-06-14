f78a4a4ecf162c0f956ae0263a263762d0428cf0
Yutocchi <shichito7@gmail.com>
2026-06-14 18:14:12 +0900

    class playerr

--- practice/yuumi/0607/script.js
+++ practice/yuumi/0607/script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const character = new Image();
 character.src = "../images/BlobStand.png";
 const SPEED = 4.0;
-const SCROLL_SPEED = 2.0;
+const SCROLL_SPEED = 7.0;
 const THEGROUND = 400 - 51 + 8;
 
 
@@ -32,6 +32,9 @@ class Player {
     y;
     width;
     height;
+    vy;
+    ay;
+    isGround;
 
     constructor(image, x, y, w, h){
         this.image = image;
@@ -39,25 +42,59 @@ class Player {
         this.y = y;
         this.width = w;
         this.height = h;
+        this.vy = 0;
+        this.ay = -2;
+        this.isGround = true;
     }
 
     draw(ctx){
     ctx.drawImage(this.image, this.x, 410 - this.height - this.y, this.width, this.height)
     }
+
+    //jump定義
+    jump() {
+        if (this.isGround) {
+          this.isGround = false
+          this.vy = 25;
+      }
+    }
+
+    //player情報更新定義
+    update(){
+        if (this.isGround) return; //地面ついてたらすることないので帰ってください
+        this.vy += this.ay;
+        this.y += this.vy;
+    
+        if(this.y < 0){
+            this.vy = 0;
+            this.y = 0;
+            this.isGround = true
+        }
+    }
+
+    //当たり判定
+    checkCollision(obstacle){
+        return !(
+            this.x + this.width < obstacle.x ||
+            this.x > obstacle.x + obstacle.width ||
+            this.y - this.height > obstacle.y ||
+            this.y < obstacle.y - obstacle.height
+        )
+    }
+    //  || = 又は, && = かつ
 }
 
 // let x = 5;
 // let y = 400 - 51 + 8;
 const player = new Player(character, 0, 0, 75, 51)
-let vy = 0; //y方向の速度
-let ay = 2; //y方向の加速度
-let isGround = true;
+// let vy = 0; //y方向の速度
+// let ay = 2; //y方向の加速度
 
 let frame = 0;
 
 const obstacles = [
-    new Obstacle(500, 0, 30, 90),
-    new Obstacle(650, 0, 30, 90),
+    new Obstacle(500, 0, 30, 50),
+    new Obstacle(800, 0, 30, 50),
 ];
 
 
@@ -65,6 +102,9 @@ const obstacles = [
 function tick(){
     frame++;
     // x = (x + frame / 50) % 500;
+
+    player.update();
+
 
     ctx.fillStyle = "#1a5585"
     ctx.fillRect(0, 0, 500, 400)
@@ -77,12 +117,15 @@ function tick(){
     // ctx.drawImage(character, x, y, 75, 51);
 
      for(const obstacle of obstacles){
-        obstacle.draw(ctx);
         obstacle.x -= SCROLL_SPEED;
+        obstacle.draw(ctx);
+        
+        if (player.checkCollision(obstacle)) {
+            console.log("atateruyo!");
+            return
+        }
     }
     
-    
-    playerUpdate();
 
     requestAnimationFrame(tick);
 }
@@ -94,15 +137,15 @@ requestAnimationFrame(tick)
 window.addEventListener("keydown", (e) => {
     if(e.key == "ArrowRight"){
         character.src = "../images/BlobStand.png";
-        x += SPEED
+        player.x += SPEED
     }
 
     if(e.key == "ArrowLeft"){
         character.src = "../images/BlobStandBack.png";
-        x -= SPEED
+        player.x -= SPEED
     }
     if (e.key == "ArrowUp"){
-        jump();
+        player.jump();
     }
     
 });
@@ -110,40 +153,6 @@ window.addEventListener("keydown", (e) => {
 //jsはfunctionなら呼び出した後に定義書いても大丈夫
 
 
-//当たり判定
-function checkCollision(player, obstacle){
-    return !(
-        player.x + player.width < obstacle.x ||
-        player.x > obstacle.x + obstacle.width ||
-        player.y + player.height < obstacle.y ||
-        player.y < obstacle.y + obstacle.height
-    )
-}
-//  || = 又は, && = かつ
-
-
-//jump定義
-function jump() {
-    if (isGround) {
-        isGround = false
-        vy = -25;
-
-    }
-}
-
-
-//player情報更新定義
-function playerUpdate(){
-    if (isGround) return; //地面ついてたらすることないので帰ってください
-    vy += ay;
-    y += vy;
-    
-    if(y > THEGROUND){
-        vy - 0;
-        y = THEGROUND;
-        isGround = true
-    }
-}
 
 
 //よく出てくる数とか色は名前がついてたほうがわかりやすい
